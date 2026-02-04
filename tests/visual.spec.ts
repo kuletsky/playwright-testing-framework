152f22f38d4a172c06d7ea9d86f0e5c4127f0812
kuletsky <kuletsky@gmail.com>
2026-02-03 20:38:58 -0500

    ref

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -5,21 +5,21 @@ import { stabilizeHeroCarousel } from '../utils/stabilize';
 
 test('Individuals page', async ({ page }) => {
   await page.goto('/');
-  await stabilizeHeroCarousel(page);
+  // await stabilizeHeroCarousel(page);
   await percySnapshot(page, 'Individuals page');
   expect(true).toBeTruthy();
 });
 
 test('Plan Sponsors page', async ({ page }) => {
   await page.goto('/plan-sponsors');
-  await stabilizeHeroCarousel(page);
+  // await stabilizeHeroCarousel(page);
   await percySnapshot(page, 'Plan Sponsors page');
   expect(true).toBeTruthy();
 });
 
 test('Financial Professionals page', async ({ page }) => {
   await page.goto('/financial-professionals');
-  await stabilizeHeroCarousel(page);
+  // await stabilizeHeroCarousel(page);
   await percySnapshot(page, 'Financial Professionals page');
   expect(true).toBeTruthy();
 });
