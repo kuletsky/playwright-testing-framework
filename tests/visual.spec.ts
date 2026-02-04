fc4144b75231cd714ba3f98cb5c8badb705cb053
kuletsky <kuletsky@gmail.com>
2026-02-04 12:22:53 -0500

    ref visual

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,27 +1,24 @@
 import { test, expect } from '@playwright/test';
-import percySnapshot from '@percy/playwright';
 import { stabilizeHeroCarousel } from '../utils/stabilize';
-
+const { percyScreenshot } = require("@percy/playwright");
 
 test('Individuals page', async ({ page }) => {
   await page.goto('/');
-  console.log('PERCY_SERVER_ADDRESS', process.env.PERCY_SERVER_ADDRESS);
-console.log('PERCY_TOKEN prefix', process.env.PERCY_TOKEN?.slice(0, 5));
   await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Individuals page');
+  await percyScreenshot(page, 'Individuals page', {fullpage: true });
   expect(true).toBeTruthy();
 });
 
 test('Plan Sponsors page', async ({ page }) => {
   await page.goto('/plan-sponsors');
   await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Plan Sponsors page');
+  await percyScreenshot(page, 'Plan Sponsors page', {fullpage: true });
   expect(true).toBeTruthy();
 });
 
 test('Financial Professionals page', async ({ page }) => {
   await page.goto('/financial-professionals');
   await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Financial Professionals page');
+  await percyScreenshot(page, 'Financial Professionals page', {fullpage: true });
   expect(true).toBeTruthy();
 });
