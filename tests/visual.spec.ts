d7e5bd5290f34c9be498e6a580601cae102526d3
kuletsky <kuletsky@gmail.com>
2026-02-03 23:41:24 -0500

    ref

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -1,25 +1,25 @@
 import { test, expect } from '@playwright/test';
-import percySnapshot from '@percy/playwright';
+import percyScreenshot from '@percy/playwright';
 import { stabilizeHeroCarousel } from '../utils/stabilize';
 
 
 test('Individuals page', async ({ page }) => {
   await page.goto('/');
   await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Individuals page');
+  await percyScreenshot(page, 'Individuals page');
   expect(true).toBeTruthy();
 });
 
 test('Plan Sponsors page', async ({ page }) => {
   await page.goto('/plan-sponsors');
-  await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Plan Sponsors page');
+  // await stabilizeHeroCarousel(page);
+  await percyScreenshot(page, 'Plan Sponsors page');
   expect(true).toBeTruthy();
 });
 
 test('Financial Professionals page', async ({ page }) => {
   await page.goto('/financial-professionals');
-  await stabilizeHeroCarousel(page);
-  await percySnapshot(page, 'Financial Professionals page');
+  // await stabilizeHeroCarousel(page);
+  await percyScreenshot(page, 'Financial Professionals page');
   expect(true).toBeTruthy();
 });
