23c2e78385b4d180c80f8f688966184e4952a367
kuletsky <kuletsky@gmail.com>
2026-02-04 13:23:25 -0500

    ref

--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -4,21 +4,21 @@ const { percyScreenshot } = require("@percy/playwright");
 
 test('Individuals page', async ({ page }) => {
   await page.goto('');
-  await stabilizeHeroCarousel(page);
-  await percyScreenshot(page, 'Individuals page', {fullpage: true });
+  // await stabilizeHeroCarousel(page);
+  await percyScreenshot(page, 'Individuals page', {fullPage: true });
   expect(true).toBeTruthy();
 });
 
 test('Plan Sponsors page', async ({ page }) => {
   await page.goto('/plan-sponsors');
-  await stabilizeHeroCarousel(page);
-  await percyScreenshot(page, 'Plan Sponsors page', {fullpage: true });
+  // await stabilizeHeroCarousel(page);
+  await percyScreenshot(page, 'Plan Sponsors page', {fullPage: true });
   expect(true).toBeTruthy();
 });
 
 test('Financial Professionals page', async ({ page }) => {
   await page.goto('/financial-professionals');
-  await stabilizeHeroCarousel(page);
-  await percyScreenshot(page, 'Financial Professionals page', {fullpage: true });
+  // await stabilizeHeroCarousel(page);
+  await percyScreenshot(page, 'Financial Professionals page', {fullPage: true });
   expect(true).toBeTruthy();
 });
