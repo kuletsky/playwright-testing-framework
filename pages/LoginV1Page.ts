4719163d1fe2e9dca070502e8dabd27d4164c32b
kuletsky <kuletsky@gmail.com>
2026-02-05 01:58:20 -0500

    add tests

--- pages/LoginV1Page.ts
+++ pages/LoginV1Page.ts
@@ -5,11 +5,19 @@ export class LoginV1Page {
     private page: Page;
     private loginButtonLocator: Locator
     private loginRetirementButtonLocator: Locator
+    private loginPersonalDashboardButtonLocator: Locator
+    private loginPersonalWealthButtonLocator: Locator
+    private loginRetirementPlanSponsorsButtonLocator: Locator
+    private loginRetirementPlanFinancialProfessionalsButtonLocator: Locator
 
     constructor(page: Page) {
         this.page = page;
         this.loginButtonLocator = this.page.locator("//header//span[text()='Login']");
         this.loginRetirementButtonLocator = this.page.locator("[aria-label='Log in to retirement plan']");
+        this.loginPersonalDashboardButtonLocator = this.page.locator("[aria-label='Log in to dashboard']");
+        this.loginPersonalWealthButtonLocator = this.page.locator("[aria-label='Log in to individual account']");
+        this.loginRetirementPlanSponsorsButtonLocator = this.page.locator("[aria-label='Log in as a retirement plan sponsor']");
+        this.loginRetirementPlanFinancialProfessionalsButtonLocator = this.page.locator("[aria-label='Log in as a financial professional']");
     }
 
     async gotoIndividualsPage() {
@@ -23,4 +31,18 @@ export class LoginV1Page {
     async clickLoginRetirementAccount() {
         await this.loginRetirementButtonLocator.click();
     }
+
+    async clickLoginPersonalDashboard() {
+        await this.loginPersonalDashboardButtonLocator.click();
+    }
+    async clickLoginPersonalWealth() {
+        await this.loginPersonalWealthButtonLocator.click();
+    }
+    async clickLoginRetirementPlanSponsors() {
+        await this.loginRetirementPlanSponsorsButtonLocator.click();
+    }
+    async clickLoginRetirementPlanFinancialProfessionals() {
+        await this.loginRetirementPlanFinancialProfessionalsButtonLocator.click();
+    }
+
 }
--- tests/login-v1-function.spec.ts
+++ tests/login-v1-function.spec.ts
@@ -12,3 +12,39 @@ test('Login Retirement account', async ({ page }) => {
 
     await expect(page).not.toHaveURL('/login-v1');
 });
+
+test('Login Personal Dashboard', async ({ page }) => {
+    const loginPage = new LoginV1Page(page);
+    await loginPage.gotoIndividualsPage();
+    await loginPage.clickLoginButton();
+    await loginPage.clickLoginPersonalDashboard();
+
+    await expect(page).not.toHaveURL('/login-v1');
+});
+
+test('Login Personal Wealth', async ({ page }) => {
+    const loginPage = new LoginV1Page(page);
+    await loginPage.gotoIndividualsPage();
+    await loginPage.clickLoginButton();
+    await loginPage.clickLoginPersonalWealth();
+
+    await expect(page).not.toHaveURL('/login-v1');
+});
+
+test('Login Retirement plan sponsors', async ({ page }) => {
+    const loginPage = new LoginV1Page(page);
+    await loginPage.gotoIndividualsPage();
+    await loginPage.clickLoginButton();
+    await loginPage.clickLoginRetirementPlanSponsors();
+
+    await expect(page).not.toHaveURL('/login-v1');
+});
+
+test('Login Retirement plan financial professionals', async ({ page }) => {
+    const loginPage = new LoginV1Page(page);
+    await loginPage.gotoIndividualsPage();
+    await loginPage.clickLoginButton();
+    await loginPage.clickLoginRetirementPlanFinancialProfessionals();
+
+    await expect(page).not.toHaveURL('/login-v1');
+});
